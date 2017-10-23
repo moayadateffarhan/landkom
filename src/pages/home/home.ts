fd3230fe9cc7dcc6e27742b43882aa97da023da6
Moayad Farhan <mfarhan@cashU.local>
2017-10-23 09:44:35 +0300

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,11 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  cucumber: boolean;
   constructor(public navCtrl: NavController) {
 
   }
-
+  updateCucumber() {
+    console.log('Cucumbers new state:' + this.cucumber);
+  }
 }
